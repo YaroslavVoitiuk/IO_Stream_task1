9c727110adce638f52466977e5de830664c01b4c
Yaroslav Voitiuk <yaroslav.voitiuk@gmail.com>
2021-07-21 11:53:58 +0300

    task fixed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args){
 
         StringBuilder sb = new StringBuilder();
 
@@ -26,33 +29,48 @@ public class Main {
         File utilsJava = new File(main, "Utils.java");
         File tempFile = new File(temp,"temp.txt");
 
-        if(src.mkdir() && res.mkdir() && savegames.mkdir() && temp.mkdir() && main.mkdir() && test.mkdir() &&
-        drawables.mkdir() && vectors.mkdir() && icons.mkdir()){
-            sb.append("* Директории были успешно созданы\n");
+        List<File> directories = Arrays.asList(src,res,savegames,temp,main,test,drawables,vectors,icons);
+        for (File d:directories) {
+            if(d.mkdir()) {
+                sb.append("Директория " + d.getName() + " была успешно создана\n");
+            } else {
+                sb.append("Директория " + d.getName() + " уже существует или произошла ошибка\n");
+            }
         }
 
+//        if(src.mkdir() && res.mkdir() && savegames.mkdir() && temp.mkdir() && main.mkdir() && test.mkdir() &&
+//        drawables.mkdir() && vectors.mkdir() && icons.mkdir()){
+//            sb.append("* Директории были успешно созданы\n");
+//        }
+
         try {
-            if (mainJava.createNewFile() && utilsJava.createNewFile() && tempFile.createNewFile()) {
-                sb.append("* Файлы были успешно созданы\n");
+            List<File> files = Arrays.asList(mainJava,utilsJava,tempFile);
+            for (File f:files) {
+                if (f.createNewFile()) {
+                    sb.append("Файл " + f.getName() + " был успешно создан\n");
+                } else {
+                    sb.append("Файл " + f.getName() + " уже существует или произошла ошибка\n");
+                }
             }
+            sb.append("END_LOG\n");
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
 
         try {
-            FileWriter fw = new FileWriter(tempFile,false);
+            FileWriter fw = new FileWriter(tempFile,true);
             fw.write(sb.toString());
             fw.close();
         } catch (IOException e) {
-            e.getMessage();
+            System.out.println(e.getMessage());
         }
 
         try {
             FileReader fr = new FileReader(tempFile);
-                int c;
-                while((c = fr.read()) != -1 ){
-                    System.out.print((char) c);
-                }
+            int c;
+            while((c = fr.read()) != -1 ){
+                System.out.print((char) c);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
